999c049f1b639c8df620dddfec63e7ad93bb2efe
reiko <99838600+nouurdm@users.noreply.github.com>
2026-04-02 17:17:41 +0200

    Delete zixflowdez/cookie-hugging-time directory

--- zixflowdez/cookie-hugging-time/keyboard.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("keydown", (e) => {
-    key.innerHTML = `<h1>${e.key}</h1>`
-
-    if(e.key === "k" || e.key === "K"){
-        key.innerHTML = `<h1>${e.key} COOKIIIIIIE</h1>`
-        audio.play()
-    }
-})
